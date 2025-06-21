022c3de9b40a5fc8e3a6b8877a2e75db28ecb041
marcostorm <mhstorms@gmail.com>
2025-06-21 18:05:57 -0500

    Implementação da busca de dados dos produtos

---
+++ src/app/modules/products/products.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router"
+import { ProductsHomeComponent } from "./page/products-home/products-home.component"
+
+export const PRODUCT_ROUTES: Routes = [
+  {
+    path: '',
+    component: ProductsHomeComponent,
+  }
+]
